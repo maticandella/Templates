b998fb50a2c59700a307144733bfb8b4f453404d
maticandella <matiascandella94@gmail.com>
2025-05-25 13:09:42 -0300

    ajuste vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import icon from 'astro-icon'
+import icon from 'astro-icon';
+import vercel from '@astrojs/vercel/serverless'; 
 
 // https://astro.build/config
 export default defineConfig({
+    adapter: vercel({}),
     integrations: [icon()]
 });
